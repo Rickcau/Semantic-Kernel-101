1207b50192a6a3b5a078b05a5d71b2a56fb1636b
Rick Caudle <rickcau@hotmail.com>
2024-03-29 16:55:39 -0400

    done

---
+++ solutions/Semantic-Kernel-101/ConsoleApp-SK-Lesson-1/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel;
+using System.Configuration;
+
+
+Console.WriteLine("Our first Console App - Semantic Kernel Chat Bot");
+
+var builder = Kernel.CreateBuilder();
+
+var openAiDeployment = ConfigurationManager.AppSettings.Get("AzureOpenAIModel");
+var openAiUri = ConfigurationManager.AppSettings.Get("AzureOpenAIEndpoint");
+var openAiApiKey = ConfigurationManager.AppSettings.Get("AzureOpenAIKey");
+
+if (openAiDeployment != null && openAiUri != null && openAiApiKey != null)
+{
+    builder.Services.AddAzureOpenAIChatCompletion(
+    deploymentName: openAiDeployment,
+    endpoint: openAiUri,
+    apiKey: openAiApiKey);
+
+}
+
+var kernel = builder.Build();
+
+ChatHistory history = [];
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+while (true)
+{
+    Console.Write(">> ");
+    var userMessage = Console.ReadLine();
+    if (userMessage != "Exit")
+    {
+        history.AddUserMessage(userMessage!);
+
+        // Not really being used in this example but we will use it in future examples
+        OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+        {
+            ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+        };
+
+        try
+        {
+            var result = await chatCompletionService.GetChatMessageContentAsync(
+                history,
+                executionSettings: openAIPromptExecutionSettings,
+                kernel: kernel);
+
+            Console.WriteLine("<< " + result);
+
+            if (result.Content != null)
+            {
+                history.AddAssistantMessage(result.Content);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+    else break;
+}
+
+
