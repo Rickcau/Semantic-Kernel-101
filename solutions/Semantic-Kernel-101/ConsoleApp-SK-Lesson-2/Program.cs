92c9aa01d31b16e2c9e701071645bcd375f97e0c
Rick Caudle <rickcau@hotmail.com>
2024-06-20 16:01:38 -0400

    Added a new lesson to demostrate how to use the DataSources Extention with ChatCompletion

--- solutions/Semantic-Kernel-101/ConsoleApp-SK-Lesson-2/Program.cs
+++ solutions/Semantic-Kernel-101/ConsoleApp-SK-Lesson-2/Program.cs
@@ -9,7 +9,7 @@ using Console_SK_Planner.Plugins;
 
 
 Console.WriteLine("Our first Console App - Semantic Kernel Chat Bot\n");
-var runExercise = 5;
+var runExercise = 6;
 // 5 Simple steps to use the Kernel!
 
 #region Step 1: Create Kernel Builder
@@ -22,6 +22,12 @@ var builder = Kernel.CreateBuilder();
 var openAiDeployment = ConfigurationManager.AppSettings.Get("AzureOpenAIModel");
 var openAiUri = ConfigurationManager.AppSettings.Get("AzureOpenAIEndpoint");
 var openAiApiKey = ConfigurationManager.AppSettings.Get("AzureOpenAIKey");
+var aiSearchUri = ConfigurationManager.AppSettings.Get("AzureSearchEndpoint");
+var aiSearchApiKey = ConfigurationManager.AppSettings.Get("AzureSearchApiKey");
+var aiSearchIndex = ConfigurationManager.AppSettings.Get("AzureSearchIndex");
+var aiSearchSemanticConfig = ConfigurationManager.AppSettings.Get("AzureSearchSemanticConfig");
+// AzureSearchSemanticConfig
+// AzureSearchApiKey
 
 #endregion
 
@@ -187,6 +193,93 @@ if (runExercise == 5)
         }
         else break;
     }
+}
+
+if (runExercise == 6)
+{
+    // Important Breaking Changes:  https://learn.microsoft.com/en-us/azure/ai-services/openai/references/on-your-data?tabs=python
+    // In this example we are going to use the ChatCompletion Extension endpoint to allow us to chat with our data.
+    // For simple cases this allows uss to allow the AI to call the AI Search without us having to use the AI Search Client.
+
+    // 1st we need to properly setup the OpenAIPrmptExecutionSettings object for this.
+#pragma warning disable SKEXP0010
+    var dataConfig = new AzureOpenAIChatCompletionWithDataConfig
+    {
+        CompletionModelId = openAiDeployment ?? "",
+        CompletionEndpoint = openAiUri ?? "",
+        CompletionApiKey = openAiApiKey ?? "",
+        DataSourceEndpoint = aiSearchUri ?? "",
+        DataSourceApiKey = aiSearchApiKey ?? "",
+        DataSourceIndex = aiSearchIndex ?? ""
+    };
+
+    // Why can I not simply use ChatCompletionServiceExtensions.GetChatMessageContentAsync()
+    // I am simply trying to replicate what I can do via Postman and passing the data_sources in the request body
+    // If I can get the same type of results using this, then I don't have to use an AI Search client
+
+
+    // var test = ChatCompletionServiceExtensions.GetChatCompletionTest(dataConfig);
+
+    var chatCompletion = new AzureOpenAIChatCompletionWithDataService(dataConfig);
+
+    var dataSources = new[]
+        {
+            new
+            {
+                type = "azure_search",
+                parameters = new
+                {
+                    endpoint = aiSearchUri,
+                    index_name = aiSearchIndex,
+                    semantic_configuration = aiSearchSemanticConfig,
+                    query_type = "vectorSemanticHybrid",
+                    fields_mapping = new { },
+                    in_scope = true,
+                    role_information = "You are an AI assistant that helps people find information.",
+                    filter = "",
+                    strictness = 3,
+                    top_n_documents = 5,
+                    authentication = new
+                     {
+                         type = "api_key",
+                         key = aiSearchApiKey
+                     },
+                    embedding_dependency = new
+                     {
+                       type = "deployment_name",
+                       deployment_name = "rdc-text-embedding"
+                     },
+                    key = aiSearchApiKey,
+                    indexName = aiSearchIndex
+                }
+            }
+        };
+    var settings = new OpenAIPromptExecutionSettings
+    {
+        ChatSystemPrompt = "You are an AI assistant that helps people find information.",
+        MaxTokens = 800,
+        ModelId = openAiDeployment,
+        Temperature = 0,
+        TopP = 0.7,
+        FrequencyPenalty = 0,
+        PresencePenalty = 0,
+        
+        ExtensionData = new Dictionary<string, object>
+            {
+                { "dataSources", dataSources }
+            },
+    };
+
+    ChatHistory chat = new();
+    chat.AddUserMessage("What are my healthcare benefits");
+
+
+    // Chat Completion example
+    var chatMessage = (AzureOpenAIWithDataChatMessageContent)await chatCompletion.GetChatMessageContentAsync(chat,settings);
+
+    var response = chatMessage.Content!;
+    var toolResponse = chatMessage.ToolContent;
+
 }
 
 
